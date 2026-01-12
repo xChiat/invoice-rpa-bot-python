80395b3dc4813edbd4f8a546672516d09bc1682d
Cristian Juble <chiat.contact@gmail.com>
2026-01-11 22:56:47 -0300

    limpiando

--- src/ai_extraction.py
+++ src/ai_extraction.py
@@ -5,3 +5,5 @@ load_dotenv()
 HF_TOKEN = os.getenv("HUGGINGFACE_TOKEN")
 if not HF_TOKEN:
     raise ValueError("HUGGINGFACE_TOKEN no encontrado en .env. Crea uno en huggingface.co/settings/tokens.")
+
+# TODO Extraccion inteligente de campos clave
